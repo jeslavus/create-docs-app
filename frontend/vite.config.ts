9e53059a4e52c7d599659382395f8cb2b240d0e3
jeslavus <jeslavus@gmail.com>
2024-08-26 19:45:45 +0700

    testing front

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -12,6 +12,15 @@ export default defineConfig({
     vueJsx(),
     vueDevTools(),
   ],
+  server: {
+    proxy: {
+      '/generate-document': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
